996980d5651b94f4c94db0120f736a8a5dd631bf
sunnysnow416 <570507051@qq.com>
2020-08-12 23:17:59 +0800

    demo 增删改查

---
+++ src/main/java/com/sunnysnow/dao/IUserDao.java
@@ -0,0 +1,37 @@
+package com.sunnysnow.dao;
+
+import com.sunnysnow.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户的持久层接口
+ */
+public interface IUserDao {
+    /**
+     * 查询所有
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据id删除用户
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+}
+
+
+
